f4378ff966c56043c727ca56079e140a06d4721e
Lukáš Vajda <lukasvajda@icloud.com>
2016-08-02 16:09:28 +0200

    - added: Framework version 1.3.1 - added: Sample in Swift version 1.3 - added: Sample in Objective-C version 1.3.1

---
+++ Sample in Objective-C/SpeedcheckerSDKSample-objc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SpeedcheckerSDKSample-objc
+//
+//  Created by Lukáš Vajda on 22.07.16.
+//  Copyright © 2016 Speedchecker Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
