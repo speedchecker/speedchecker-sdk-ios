ee183e9897ce75297d8c928dc4d13cbd250c1cf7
Lukáš Vajda <lukasvajda@icloud.com>
2019-04-23 16:48:04 +0200

    Update of the Sample app

---
+++ Sample in Objective-C/SpeedcheckerSDKSample-objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpeedcheckerSDKSample-objc
+//
+//  Created by Lukáš Vajda on 22.07.16.
+//  Copyright © 2016 Speedchecker Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample in Objective-C/SpeedcheckerSDKSample-objc/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SpeedcheckerSDKSample-objc
+//
+//  Created by Lukáš Vajda on 22.07.16.
+//  Copyright © 2016 Speedchecker Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpeedcheckerSDK/SpeedcheckerSDK-Swift.h>
+
+@interface ViewController : UIViewController <SpeedTestDelegate>
+
+
+@end
+
