42aacb9a7b62178b2b7ccab1bf4058bff245a9b1
Valerii <vandrusik@gmail.com>
2020-08-06 09:40:03 +0300

    Version 1.5.3, Xcode 11.6

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Speedchecker SDK for iOS, version 1.13
+# Speedchecker SDK for iOS, version 1.5.3
 
 The embedded framework version of the Speedchecker Speed Test supports its own API which allows you to access various methods and events easily from the your iOS application code. You can also try our  [Speedchecker on App Store](https://itunes.apple.com/app/id658790195), it's powered by the latest Speedchecker SDK version.
 
 ## Features
-- latency, downlad and upload speed of the user connection
+- latency, download and upload speed of the user connection
 - robust measuring of cellular, wireless, even local network
 - testing details like the current speed and progress
 - additional information like network type and location
@@ -11,14 +11,13 @@ The embedded framework version of the Speedchecker Speed Test supports its own A
 - detailed statistics and reports by Speedchecker
 
 ## Requirements
-  - Xcode 10.2 or later
-  - Swift 5 or Objective-C
-  - Development Target 10.0 or later
-  - Bundle Identifier licensed by Speedchecker Ltd.
+  - Xcode 11.6 or later
+  - Swift 5
+  - Development Target 12.1 or later
 
 ## Installation
-1. Place the SpeedcheckerSDK.framework into your Xcode project. We recommend you to copy it rather than to reference from an other location.
-2. In your application target, go to General tab and drag SpeedcheckerSDK.framework item into Embeded Binaries section. So than you should see it in the Embeded Binaries and also in the Linked Frameworks and Libraries sections. 
+1. Place the SpeedcheckerSDK.framework and all other frameworks from SDK folder into your Xcode project. We recommend you to copy it rather than to reference from an other location.
+2. In your application target, go to General tab, section Frameworks, Libraries, and Embedded Content, and set "Embed & Sign" for all added frameworks.
 3. Go to Build Phases tab and recheck it’s added in Link Binary With Libraries and also in the Embedded Frameworks section.
 4. Now the SpeedcheckerSDK framework should be importable to your code.
 	#### Swift
@@ -26,16 +25,10 @@ The embedded framework version of the Speedchecker Speed Test supports its own A
     import SpeedcheckerSDK
     ```
     
-    #### Objective-C
-    ```objc
-    #import "SpeedcheckerSDK/SpeedcheckerSDK.h"
-    #import "SpeedcheckerSDK/SpeedcheckerSDK-Swift.h"
-    ```
 
 ## What's next?
 Please contact us for future details and license requirements. Also you can download the latest framework version, the sample app to see detailed implementation in Xcode project as well as our Internet Speed Test application on App Store.
 - [Contact us](http://www.speedchecker.xyz/contact-us.html)
 - [Speedchecker on App Store](https://itunes.apple.com/app/id658790195)
-- [Framework](https://github.com/speedchecker/speedchecker-sdk-ios/tree/master/Framework/SpeedcheckerSDK.framework)
-- [Sample app in Swift](https://github.com/speedchecker/speedchecker-sdk-ios/tree/master/Sample%20in%20Objective-C)
-- [Sample app in Objective-C](https://github.com/speedchecker/speedchecker-sdk-ios/tree/master/Sample%20in%20Swift)
+- [Framework](https://github.com/speedchecker/speedchecker-sdk-ios/tree/master/Framework)
+- [Sample app in Swift](https://github.com/speedchecker/speedchecker-sdk-ios/tree/master/Sample%20in%20Swift)
--- Sample in Swift/SpeedcheckerSDK Sample/ViewController.swift
+++ Sample in Swift/SpeedcheckerSDK Sample/ViewController.swift
@@ -2,142 +2,82 @@
 //  ViewController.swift
 //  SpeedcheckerSDK Sample
 //
-//  Created by Lukáš Vajda on 18.07.16.
-//  Copyright © 2016 Speedchecker Ltd. All rights reserved.
 //
 
 import UIKit
 import SpeedcheckerSDK
+import CoreLocation
 
-class ViewController: UIViewController, SpeedTestDelegate {
-	
-	var speedTest: SpeedTestController?
-	
-	// Outlets
-	@IBOutlet weak var serverValue: UILabel!
-	@IBOutlet weak var latencyValue: UILabel!
-	
-	@IBOutlet weak var downloadValue: UILabel!
-	@IBOutlet weak var downloadProgress: UIProgressView!
-	
-	@IBOutlet weak var uploadValue: UILabel!
-	@IBOutlet weak var uploadProgress: UIProgressView!
-	
-	@IBOutlet weak var ispValue: UILabel!
-	@IBOutlet weak var ipValue: UILabel!
-	
-	// Actions
-	@IBAction func startButton_didTouch(sender: UIButton) {
-		speedTest?.start() { error in
-			if (error != .None) {
-				return print("speedTest did fail: \(error.rawValue)")
-			}
-			
-			dispatch_async(dispatch_get_main_queue()) {
-				self.resetUI()
-			}
-		}
-	}
-	
-	// Events
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-		
-		speedTest = SpeedTestController(clientID: 49, delegate: self)
-		
-		resetUI()
-	}
-	
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-	
-	// Helper
-	func resetUI() {
-		serverValue.text = nil
-		latencyValue.text = nil
-		
-		downloadValue.text = nil
-		downloadProgress.setProgress(0, animated: false)
-		
-		uploadValue.text = nil
-		uploadProgress.setProgress(0, animated: false)
-		
-		self.ispValue.text = nil
-		self.ipValue.text = nil
-	}
-	
-	// Delegate: SpeedTestDelegate
-	func speedTestDidReceiveServers(servers: [SpeedTestServer]) {
-		print("speedTestDidReceiveServers \(servers.count)")
-	}
-	
-	func speedTestDidSelectServer(server: SpeedTestServer, withLatencyInMs latency: Int) {
-		dispatch_async(dispatch_get_main_queue()) {
-			self.serverValue.text = server.domain
-			self.latencyValue.text = "\(latency) ms"
-		}
-	}
-	
-	func speedTestDownloadDidStart() {
-		print("speedTestDownloadDidStart")
-	}
-	
-	func speedTestDownloadDidFinish() {
-		print("speedTestDownloadDidFinish")
-	}
-	
-	func speedTestDownloadDidProgress(progress: Double, withSpeed speed: SpeedTestSpeed?) {
-		dispatch_async(dispatch_get_main_queue()) {
-			self.downloadProgress.setProgress(Float(progress), animated: true)
-		}
-		
-		if let speed = speed {
-			dispatch_async(dispatch_get_main_queue()) {
-				self.downloadValue.text = "\(speed.descriptionInMbps) Mb/s"
-			}
-		}
-	}
-	
-	func speedTestUploadDidStart() {
-		print("speedTestUploadDidStart")
-	}
-	
-	func speedTestUploadDidFinish() {
-		print("speedTestUploadDidFinish")
-	}
-	
-	func speedTestUploadDidProgress(progress: Double, withSpeed speed: SpeedTestSpeed?) {
-		dispatch_async(dispatch_get_main_queue()) {
-			self.uploadProgress.setProgress(Float(progress), animated: true)
-		}
-		
-		if let speed = speed {
-			dispatch_async(dispatch_get_main_queue()) {
-				self.uploadValue.text = "\(speed.descriptionInMbps) Mb/s"
-			}
-		}
-	}
-	
-	func speedTestDidFinish(withResult result: SpeedTestResult) {
-		print("speedTestDidFinish result: \(result)")
-		
-		dispatch_async(dispatch_get_main_queue()) {
-			self.serverValue.text = result.server.domain
-			self.latencyValue.text = "\(result.latencyInMs) ms"
-			
-			self.downloadValue.text = "\(result.downloadSpeed.descriptionInMbps) Mb/s"
-			self.uploadValue.text = "\(result.uploadSpeed.descriptionInMbps) Mb/s"
-			
-			self.ispValue.text = result.ispName
-			self.ipValue.text = result.ipAddress
-		}
-	}
-	
-	func speedTestDidFinish(withError error: SpeedTestError) {
-		print("speedTestDidFinish error: \(error.rawValue)")
-	}
+class ViewController: UIViewController {
+
+    private var internetTest: InternetSpeedTest?
+    private var locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func runSpeedTestTouched(_ sender: UIButton) {
+        // to use free version, your app should have location access
+        internetTest = InternetSpeedTest(delegate: self)
+        internetTest?.startTest() { (error) in
+            print(error)
+        }
+        
+        // to use paid version, your app does not need location access
+//        internetTest = InternetSpeedTest(clientID: 0, userID: 0, delegate: self)
+//        internetTest?.start() { (error) in
+//            print(error)
+//        }
+    }
+    
+}
+
+extension ViewController: InternetSpeedTestDelegate {
+    func internetTest(finish error: SpeedTestError) {
+        print(error)
+    }
+    
+    func internetTest(finish result: SpeedTestResult) {
+        print(result.downloadSpeed.mbps)
+        print(result.uploadSpeed.mbps)
+        print(result.latencyInMs)
+        
+    }
+    
+    func internetTest(received servers: [SpeedTestServer]) {
+        //
+    }
+    
+    func internetTest(selected server: SpeedTestServer, latency: Int) {
+        //
+    }
+    
+    func internetTestDownloadStart() {
+        //
+    }
+    
+    func internetTestDownloadFinish() {
+        //
+    }
+    
+    func internetTestDownload(progress: Double, _ speed: SpeedTestSpeed?) {
+        //
+    }
+    
+    func internetTestUploadStart() {
+        //
+    }
+    
+    func internetTestUploadFinish() {
+        //
+    }
+    
+    func internetTestUpload(progress: Double, _ speed: SpeedTestSpeed?) {
+        //
+    }
+    
+    
 }
 
