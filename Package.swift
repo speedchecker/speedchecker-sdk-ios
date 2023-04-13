b25809aab54273e6495b440383ab5d849724a907
Nakaoka Rei <reideeplearning@gmail.com>
2023-04-14 04:20:23 +0900

    Support SPM for xcframework of Xcode14.1 (#19)
    * feat: add package.swift
    
    * Update Package.swift
    
    ---------
    
    Co-authored-by: bogdanicc <predragbogdanicc@gmail.com>

---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SpeedcheckerSDK",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SpeedcheckerSDK",
+            targets: ["SpeedcheckerSDK", "SpeedcheckerReportSDK", "XMLParsing", "Socket", "DataCompression"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "SpeedcheckerSDK",
+            path: "Framework/xcode14.1/SpeedcheckerSDK.xcframework"
+        ),
+        .binaryTarget(
+            name: "SpeedcheckerReportSDK",
+            path: "Framework/xcode14.1/SpeedcheckerReportSDK.xcframework"
+        ),
+        .binaryTarget(
+            name: "XMLParsing",
+            path: "Framework/xcode14.1/XMLParsing.xcframework"
+        ),
+        .binaryTarget(
+            name: "Socket",
+            path: "Framework/xcode14.1/Socket.xcframework"
+        ),
+        .binaryTarget(
+            name: "DataCompression",
+            path: "Framework/xcode14.1/DataCompression.xcframework"
+        ),
+    ]
+)
