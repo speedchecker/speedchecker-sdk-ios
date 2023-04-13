a15cb8c5a2dad57ac721ecb3b12d3136f7178de9
bogdanicc <predragbogdanicc@gmail.com>
2023-04-13 21:19:35 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SpeedcheckerSDK-spm",
+    name: "SpeedcheckerSDK",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
